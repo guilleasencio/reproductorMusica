ec724596043956e3dd9fe378c04c9e306a1bb4c9
Guillermo Asencio Sanchez <Willy@macbook-pro-de-guillermo.local>
2016-04-14 17:59:49 +0200

    Initial Commit

---
+++ reproductorMusica/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  reproductorMusica
+//
+//  Created by Guillermo Asencio Sanchez on 14/4/16.
+//  Copyright © 2016 Guillermo Asencio Sanchez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
